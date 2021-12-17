2c5c6dcc9df88fadf0c5689a9721f6936a89d6a3
gsh1150520575@163.com <gsh1150520575@163.com>
2021-12-17 20:31:19 +0800

    update

--- examination_system.sql
+++ examination_system.sql
@@ -1,185 +1,222 @@
-/*
-SQLyog Ultimate v12.14 (64 bit)
-MySQL - 5.5.40 : Database - examination_system
-*********************************************************************
-*/
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: examination_system
+-- ------------------------------------------------------
+-- Server version	8.0.26
 
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`examination_system` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
 
-USE `examination_system`;
-
-/*Table structure for table `college` */
+--
+-- Table structure for table `college`
+--
 
 DROP TABLE IF EXISTS `college`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `college` (
-  `collegeID` int(11) NOT NULL,
+  `collegeID` int NOT NULL,
   `collegeName` varchar(200) NOT NULL COMMENT '课程名',
   PRIMARY KEY (`collegeID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*Data for the table `college` */
+--
+-- Dumping data for table `college`
+--
 
-insert  into `college`(`collegeID`,`collegeName`) values 
-(1,'计算机系'),
-(2,'设计系'),
-(3,'财经系');
+LOCK TABLES `college` WRITE;
+/*!40000 ALTER TABLE `college` DISABLE KEYS */;
+INSERT INTO `college` (`collegeID`, `collegeName`) VALUES (1,'计算机系'),(2,'数学系'),(3,'物理系');
+/*!40000 ALTER TABLE `college` ENABLE KEYS */;
+UNLOCK TABLES;
 
-/*Table structure for table `course` */
+--
+-- Table structure for table `course`
+--
 
 DROP TABLE IF EXISTS `course`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `course` (
-  `courseID` int(11) NOT NULL,
+  `courseID` int NOT NULL,
   `courseName` varchar(200) NOT NULL COMMENT '课程名称',
-  `teacherID` int(11) NOT NULL,
+  `teacherID` int NOT NULL,
   `courseTime` varchar(200) DEFAULT NULL COMMENT '开课时间',
   `classRoom` varchar(200) DEFAULT NULL COMMENT '开课地点',
-  `courseWeek` int(200) DEFAULT NULL COMMENT '学时',
+  `courseWeek` int DEFAULT NULL COMMENT '学时',
   `courseType` varchar(20) DEFAULT NULL COMMENT '课程类型',
-  `collegeID` int(11) NOT NULL COMMENT '所属院系',
-  `score` int(11) NOT NULL COMMENT '学分',
+  `collegeID` int NOT NULL COMMENT '所属院系',
+  `score` int NOT NULL COMMENT '学分',
   PRIMARY KEY (`courseID`),
   KEY `collegeID` (`collegeID`),
   KEY `teacherID` (`teacherID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*Data for the table `course` */
+--
+-- Dumping data for table `course`
+--
 
-insert  into `course`(`courseID`,`courseName`,`teacherID`,`courseTime`,`classRoom`,`courseWeek`,`courseType`,`collegeID`,`score`) values 
-(1,'C语言程序设计',1001,'周二','科401',18,'必修课',1,3),
-(2,'Python爬虫技巧',1001,'周四','X402',18,'必修课',1,3),
-(3,'数据结构',1001,'周四','科401',18,'必修课',1,2),
-(4,'Java程序设计',1002,'周五','科401',18,'必修课',1,2),
-(5,'英语',1002,'周四','X302',18,'必修课',2,2),
-(6,'服装设计',1003,'周一','科401',18,'选修课',2,2);
+LOCK TABLES `course` WRITE;
+/*!40000 ALTER TABLE `course` DISABLE KEYS */;
+INSERT INTO `course` (`courseID`, `courseName`, `teacherID`, `courseTime`, `classRoom`, `courseWeek`, `courseType`, `collegeID`, `score`) VALUES (1,'计算机组成原理',1001,'周一','6#307',17,'必修课',1,3),(2,'操作系统',1002,'周二','6#304',17,'必修课',1,3),(3,'javaweb程序设计',1004,'周二','信东119',17,'必修课',1,2),(4,'微机原理与接口技术',1003,'周三','6#309',17,'必修课',1,2),(5,'高等代数',1005,'周四','6#320',17,'必修课',1,4),(6,'原子物理学',1006,'周五','6教112',17,'必修课',3,3),(7,'大学物理实验',1006,'周二','物理实验室',17,'必修课',3,1);
+/*!40000 ALTER TABLE `course` ENABLE KEYS */;
+UNLOCK TABLES;
 
-/*Table structure for table `role` */
+--
+-- Table structure for table `role`
+--
 
 DROP TABLE IF EXISTS `role`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `role` (
-  `roleID` int(11) NOT NULL,
+  `roleID` int NOT NULL,
   `roleName` varchar(20) NOT NULL,
   `permissions` varchar(255) DEFAULT NULL COMMENT '权限',
   PRIMARY KEY (`roleID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*Data for the table `role` */
+--
+-- Dumping data for table `role`
+--
 
-insert  into `role`(`roleID`,`roleName`,`permissions`) values 
-(0,'admin',NULL),
-(1,'teacher',NULL),
-(2,'student',NULL);
+LOCK TABLES `role` WRITE;
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+INSERT INTO `role` (`roleID`, `roleName`, `permissions`) VALUES (0,'admin',NULL),(1,'teacher',NULL),(2,'student',NULL);
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+UNLOCK TABLES;
 
-/*Table structure for table `selectedcourse` */
+--
+-- Table structure for table `selectedcourse`
+--
 
 DROP TABLE IF EXISTS `selectedcourse`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `selectedcourse` (
-  `courseID` int(11) NOT NULL,
-  `studentID` int(11) NOT NULL,
-  `mark` int(11) DEFAULT NULL COMMENT '成绩',
+  `courseID` int NOT NULL,
+  `studentID` int NOT NULL,
+  `mark` int DEFAULT NULL COMMENT '成绩',
   KEY `courseID` (`courseID`),
   KEY `studentID` (`studentID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*Data for the table `selectedcourse` */
+--
+-- Dumping data for table `selectedcourse`
+--
 
-insert  into `selectedcourse`(`courseID`,`studentID`,`mark`) values 
-(2,10001,12),
-(1,10001,95),
-(1,10002,66),
-(2,10003,99),
-(5,10001,NULL),
-(3,10001,NULL),
-(1,10003,NULL),
-(4,10003,NULL);
+LOCK TABLES `selectedcourse` WRITE;
+/*!40000 ALTER TABLE `selectedcourse` DISABLE KEYS */;
+INSERT INTO `selectedcourse` (`courseID`, `studentID`, `mark`) VALUES (1,21,60),(4,21,0),(5,25,NULL),(1,25,34),(6,23,23);
+/*!40000 ALTER TABLE `selectedcourse` ENABLE KEYS */;
+UNLOCK TABLES;
 
-/*Table structure for table `student` */
+--
+-- Table structure for table `student`
+--
 
 DROP TABLE IF EXISTS `student`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `student` (
-  `userID` int(11) NOT NULL AUTO_INCREMENT,
+  `userID` int NOT NULL AUTO_INCREMENT,
   `userName` varchar(200) NOT NULL,
   `sex` varchar(20) DEFAULT NULL,
   `birthYear` date DEFAULT NULL COMMENT '出生日期',
   `grade` date DEFAULT NULL COMMENT '入学时间',
-  `collegeID` int(11) NOT NULL COMMENT '院系id',
+  `collegeID` int NOT NULL COMMENT '院系id',
   PRIMARY KEY (`userID`),
   KEY `collegeID` (`collegeID`)
-) ENGINE=InnoDB AUTO_INCREMENT=10008 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1914110670 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*Data for the table `student` */
+--
+-- Dumping data for table `student`
+--
 
-insert  into `student`(`userID`,`userName`,`sex`,`birthYear`,`grade`,`collegeID`) values 
-(9999,'mike1','男','1996-09-03','2019-11-13',3),
-(10001,'小红','男','2020-03-02','2020-03-02',1),
-(10002,'小绿','男','2020-03-10','2020-03-10',1),
-(10003,'小陈','女','1996-09-02','2015-09-02',2),
-(10005,'小左','女','1996-09-02','2015-09-02',2),
-(10007,'MIke','男','1996-09-02','2015-09-02',2);
+LOCK TABLES `student` WRITE;
+/*!40000 ALTER TABLE `student` DISABLE KEYS */;
+INSERT INTO `student` (`userID`, `userName`, `sex`, `birthYear`, `grade`, `collegeID`) VALUES (21,'高硕','男','2001-10-03','2019-08-31',1),(22,'邱子平','男','2000-08-07','2019-08-31',1),(23,'魏民','男','2001-04-22','2019-08-31',1),(25,'单俞润','男','2001-03-08','2019-08-31',1);
+/*!40000 ALTER TABLE `student` ENABLE KEYS */;
+UNLOCK TABLES;
 
-/*Table structure for table `teacher` */
+--
+-- Table structure for table `teacher`
+--
 
 DROP TABLE IF EXISTS `teacher`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `teacher` (
-  `userID` int(11) NOT NULL AUTO_INCREMENT,
+  `userID` int NOT NULL AUTO_INCREMENT,
   `userName` varchar(200) NOT NULL,
   `sex` varchar(20) DEFAULT NULL,
   `birthYear` date NOT NULL,
   `degree` varchar(20) DEFAULT NULL COMMENT '学历',
   `title` varchar(255) DEFAULT NULL COMMENT '职称',
   `grade` date DEFAULT NULL COMMENT '入职时间',
-  `collegeID` int(11) NOT NULL COMMENT '院系',
+  `collegeID` int NOT NULL COMMENT '院系',
   PRIMARY KEY (`userID`),
   KEY `collegeID` (`collegeID`)
-) ENGINE=InnoDB AUTO_INCREMENT=1004 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1914110523 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*Data for the table `teacher` */
+--
+-- Dumping data for table `teacher`
+--
 
-insert  into `teacher`(`userID`,`userName`,`sex`,`birthYear`,`degree`,`title`,`grade`,`collegeID`) values 
-(1001,'刘老师','女','1990-03-08','硕士','副教授','2015-09-02',2),
-(1002,'张老师','女','1996-09-02','博士','讲师','2015-09-02',1),
-(1003,'软老师','女','1996-09-02','硕士','助教','2017-07-07',1);
+LOCK TABLES `teacher` WRITE;
+/*!40000 ALTER TABLE `teacher` DISABLE KEYS */;
+INSERT INTO `teacher` (`userID`, `userName`, `sex`, `birthYear`, `degree`, `title`, `grade`, `collegeID`) VALUES (1001,'王老师','女','1990-03-08','硕士','副教授','2015-09-02',1),(1002,'夏老师','女','1990-03-08','硕士','讲师','2015-09-02',1),(1003,'朱老师','女','1990-03-08','硕士','助教','2017-07-07',1),(1004,'刘老师','女','1990-03-08','硕士','教授','2016-12-14',1),(1005,'张老师','男','1990-03-08','硕士','教授','2021-12-02',2),(1006,'陈老师','男','1990-03-08','硕士','副教授','2021-12-08',3);
+/*!40000 ALTER TABLE `teacher` ENABLE KEYS */;
+UNLOCK TABLES;
 
-/*Table structure for table `userlogin` */
+--
+-- Table structure for table `userlogin`
+--
 
 DROP TABLE IF EXISTS `userlogin`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `userlogin` (
-  `userID` int(11) NOT NULL AUTO_INCREMENT,
+  `userID` int NOT NULL AUTO_INCREMENT,
   `userName` varchar(200) NOT NULL,
   `password` varchar(200) NOT NULL,
-  `role` int(11) NOT NULL DEFAULT '2' COMMENT '角色权限',
+  `role` int NOT NULL DEFAULT '2' COMMENT '角色权限',
   PRIMARY KEY (`userID`),
   KEY `role` (`role`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*Data for the table `userlogin` */
+--
+-- Dumping data for table `userlogin`
+--
 
-insert  into `userlogin`(`userID`,`userName`,`password`,`role`) values 
-(1,'admin','123',0),
-(10,'10003','123',2),
-(11,'10005','123',2),
-(14,'1001','123',1),
-(15,'1002','123',1),
-(16,'1003','123',1),
-(20,'9999','123',2),
-(21,'10001','123',2),
-(22,'10002','123',2);
+LOCK TABLES `userlogin` WRITE;
+/*!40000 ALTER TABLE `userlogin` DISABLE KEYS */;
+INSERT INTO `userlogin` (`userID`, `userName`, `password`, `role`) VALUES (1,'admin','admin',0),(2,'21','21',2),(3,'22','22',2),(4,'23','23',2),(5,'25','25',2),(6,'1001','1001',1),(7,'1002','1002',1),(8,'1003','1003',1),(9,'1004','1004',1),(10,'1005','1005',1),(11,'1006','1006',1);
+/*!40000 ALTER TABLE `userlogin` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-12-17 20:31:03
